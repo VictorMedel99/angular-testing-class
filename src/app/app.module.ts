915ce058f1467c9161d4c36af16fd57630082fc9
VictorMedel99 <victor.medelito@gmail.com>
2020-10-10 19:05:24 -0500

    add compute.ts and add compute-demo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComputeDemoComponent } from './compute-demo/compute-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComputeDemoComponent
   ],
   imports: [
     BrowserModule,
